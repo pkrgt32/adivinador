7386d78de68ce6387db5d2ea9a56d0536689391f
GIBRAN  MELENDEZ <gamey305@gmail.com>
2024-02-07 22:17:03 -0600

    falta ajustes

--- app.js
+++ app.js
@@ -1,116 +1,126 @@
 let parafo = document.getElementById("respuesta");
 let preguntas = document.getElementById('input');
+let titulo = document.getElementById("h1");
+let losdig = document.getElementById("losdig")
 
 
-let partesDePregunta = ["por que", "como", "cuando",  "Dónde",  "que","por quien","quienes", "cual","quienrs","cuales","cuantos","cuanto","De quién"
+
+let preguNoAptas = ["por que", "como", "cuando",  "Dónde",  "que","por quien","quienes", "cual","quienrs","cuales","cuantos","cuanto","De quién"
 ];
-
+let preguAmables = ["como estas","como te encuentra","como te a ido"]
+let preguDeGroseras = ["te odio"]
 const ERROR = /[A-Z!@#$%^&*()_+?<>]/;
 
+
+parafo.style.display = "none"
+losdig.style.display = "none"
 
 
 
 
 function botonR() {
+    let preguntas = document.getElementById('input'); 
     let textod = preguntas.value.trim();
+    let parafo = document.getElementById("respuesta");
+
 
     if (preguntas.value == "") {
-        parafo.textContent = "as una pregunta"         
-    } else if (partesDePregunta.some(part => textod.includes(part))){ //`Array.prototype.some()`: Este método prueba si al menos un elemento en el array cumple con la condición implementada por la función proporcionada. En este caso, la función es `(part => textod.startsWith(part))`y.String.prototype.includes()`: es para re si esite alguna palagra en todo el texto del usuario
+        parafo.textContent = "no mes as preguntado nada"    
+       animacion()
+    }else if (preguDeGroseras.some(part => textod.includes(part))){
+        parafo.textContent = "yo igual ";
+        borra()
+        animacion()
+    }else if (preguAmables.some(part => textod.includes(part))){ //`Array.prototype.some()`: Este método prueba si al menos un elemento en el array cumple con la condición implementada por la función proporcionada. En este caso, la función es `(part => textod.startsWith(part))`y.String.prototype.includes()`: es para re si esite alguna palagra en todo el texto del usuario
+        parafo.textContent = "muy bien gracias";
+        borra()
+        animacion()
+    
+    }else if (preguNoAptas.some(part => textod.includes(part))){ 
         parafo.textContent = "no soy tan majico";
         borra()
+        animacion()
+    
     }else if (ERROR.test(preguntas.value)) {
         parafo.textContent = "solo puede usar minusculas sin caracteres espesiales";
         borra()
+        animacion()
     } else {
-        repuestas(preguntas)
+        repuestas()
+        animacion()
     }
 }
 
-function repuestas(preguntas) {
-    let randon = Math.floor(Math.random() * 24) + 1;
+function repuestas() {
+    let randon = Math.floor(Math.random() * 6) + 1;
     let parafo = document.getElementById("respuesta");
 
 
     if (randon == 1) {
-        parafo.textContent = "Mi respuesta es no."
+        parafo.textContent = "tabes"
         borra()
     } else if (randon == 2) {
-        parafo.textContent = " Pregunta en otro momento"
+        parafo.textContent = " si "
         borra();
     } else if (randon == 3) {
-        parafo.textContent = "No puedo predecirlo ahora."
+        parafo.textContent = "definitibanente."
         borra();
     } else if (randon == 4) {
-        parafo.textContent = "probable mente "
+        parafo.textContent = "probable mente no "
         borra();
     } else if (randon == 5) {
-        parafo.textContent = "telo por aseguro"
+        parafo.textContent = "telo por seguro"
         borra();
-    } else if (randon == 6) {
-        parafo.textContent = "definitibamente no "
-        borra();
-    } else if (randon == 7) {
-        parafo.textContent = " Es cierto"
-        borra();
-    } else if (randon == 8) {
-        parafo.textContent = " no"
-        borra();
-    } else if (randon == 9) {
-        parafo.textContent = "  Pregunta en otro momento."
-        borra();
-    } else if (randon == 10) {
-        parafo.textContent = " Es decididamente así"
-        borra();
-    } else if (randon == 11) {
-        parafo.textContent = " No puedo predecirlo ahora."
-        borra();
-    } else if (randon == 12) {
-        parafo.textContent = "  vuelve a intentarlo.."
-        borra();
-    } else if (randon == 13) {
-        parafo.textContent = " Las perspectivas no son buenas"
-        borra();
-    } else if (randon == 14) {
-        parafo.textContent = " definitivamente"
-        borra();
-    } else if (randon == 15) {
-        parafo.textContent = " No cuentes con ello."
-        borra();
-    } else if (randon == 16) {
-        parafo.textContent = " Probablemente"
-        borra();
-    } else if (randon == 17) {
-        parafo.textContent = "  Concéntrate y vuelve a preguntar."
-        borra();
-    } else if (randon == 18) {
-        parafo.textContent = "  Mis fuentes me dicen que no."
-        borra();
-    } else if (randon == 19) {
-        parafo.textContent = " Pregunta en otro momento."
-        borra();
-    } else if (randon == 20) {
-        parafo.textContent = " Todo apunta a que sí."
-        borra();
-    } else if (randon == 21) {
-        parafo.textContent = " ...."
-        borra();
-    } else if (randon == 22) {
-        parafo.textContent = " eso es ilegal no."
-        borra();
-    } else if (randon == 23) {
-        parafo.textContent = " amiga date cuenta ."
-        borra();
-    } else if (randon == 24) {
-        parafo.textContent = " no pregunte tontera."
-        borra();
+    }else if (randon == 6) {
+    parafo.textContent = "no"
+    borra();
     } else {
-        parafo.textContent = " nooooooose."
+        parafo.textContent = "preguntame luego"
         borra();
     }
 
 }
 
 function borra() {
+ 
     preguntas.value = ""
+
 }
+
+
+function animacion() {
+    let titulo = document.getElementById("h1");
+    let parafo = document.getElementById("respuesta");
+    let losdig = document.getElementById("losdig")
+
+
+    if (titulo.style.display === "none") {
+        parafo.style.opacity = 0;
+        setTimeout(function(){
+          parafo.style.display = "none";
+          titulo.style.display = "block";
+          titulo.style.opacity = 1;
+        },1000);
+      } else {
+        titulo.style.opacity = 0;
+        losdig.style.display = "block"  
+        setTimeout(function(){
+          titulo.style.display = "none";
+          parafo.style.display = "block";
+          losdig.style.display = "none"
+          parafo.style.opacity = 1;
+        },1000);
+        setTimeout(function(){
+          parafo.style.opacity = 0;
+          losdig.style.display = "block"
+          setTimeout(function(){
+            parafo.style.display = "none";
+            titulo.style.display = "block";
+            losdig.style.display = "none"
+            titulo.style.opacity = 1;
+          },1000);
+        }, 8000);
+        
+      }
+}
+
